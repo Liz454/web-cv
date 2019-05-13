8b9750deccfea6fa619d1f7c45e75034e6f281ed
Lizzie Gasson <lizzie.gasson@iag.co.nz>
2019-05-13 15:41:48 +1200

    experimenting with jquery ui and draggable

--- photography-script.js
+++ photography-script.js
@@ -2,38 +2,47 @@
 var navHeights = 100;
 
 //on load functions
-$(function(){
-    importImages("album1");
-    layout();
+$(function() {
+  importImages("album1", 10);
+  layout();
 });
 
 //on resize functions
-$('document').on("resize", function(){
-    layout();
+$("document").on("resize", function() {
+  layout();
 });
-    
 
-
-function layout(){
-    $("#images").css("height", $(document).height()-navHeights);
+function layout() {
+  $("#images").css("height", $(document).height() - navHeights);
 }
 
-function importImages(folder){
-    var itotal = 10;
-    var imgs = $('#images').find('.images-container');
-    var iwidths;
-    
-    for (var i = 0; i < itotal; i++) {
-        imgs.append("<img class='image' src='images/photography-images/" + folder + "/" + i + ".jpg'>");
-    }
-    $('.image').css("height", $(document).height()-navHeights);
+function importImages(folder, itotal) {
+  var imgs = $("#images").find(".images-container");
+  var iwidths;
 
-    $('.image').each(function(){
-        console.log($(this)[0].clientWidth);
-        iwidths += $(this).width();
-    });
-    console.log(iwidths);
+  for (var i = 0; i < itotal; i++) {
+    imgs.append(
+      "<img class='image' src='images/photography-images/" +
+        folder +
+        "/" +
+        i +
+        ".jpg'>"
+    );
+  }
+  $(".image").css("height", $(document).height() - navHeights);
 
-    
-    // $('.images-container').css("width", )
+  $(".image").each(function() {
+    console.log($(this)[0].clientWidth);
+    iwidths += $(this).width();
+  });
+  console.log(iwidths);
+
+  // $('.images-container').css("width", )
 }
+
+//https://stackoverflow.com/questions/28576636/mouse-click-and-drag-instead-of-horizontal-scroll-bar-to-view-full-content-of-c
+$(".images-container").draggable({
+  axis: "x",
+  cursor: "move",
+  containment: "images"
+});
